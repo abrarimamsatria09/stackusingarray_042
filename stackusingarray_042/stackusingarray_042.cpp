69faab17aa3830751916efa5a9b6bb0141572676
abrarimamsatria09 <092004abrar@gmail.com>
2023-06-15 10:35:31 +0700

    membuat member public dan menambahkan data hingga 4 jika sudah penuh maka muncul tampilan stack full

--- stackusingarray_042/stackusingarray_042.cpp
+++ stackusingarray_042/stackusingarray_042.cpp
@@ -7,4 +7,24 @@ private:
 	string stack_array[5];
 	int top;
 
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack Full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << " ditambahkan(pushed)." << endl;
+
+		return element;
+	}
+
 };
